958b9cdbd0e0fbc77aa7179b44b4e5f2007a2dd3
JJSploit On Top <108234984+JJSploitOnTop@users.noreply.github.com>
2023-07-05 10:38:59 -0400

    Update
    Added a command handler

--- Destruction Simulator/Script.lua
+++ Destruction Simulator/Script.lua
@@ -1,37 +1,63 @@
-local succ, err =
-    pcall(
-    function()
-        game:GetService("ReplicatedStorage").Remotes.sellBricks:FireServer()
-    end
-)
+-- Untested create something under issues and I'll fix your bug
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remotes = ReplicatedStorage.Remotes
+local Player = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Chatted = LocalPlayer.Chatted
+local Prefix = ";"
 
-if not succ then
-    warn("Sell bricks failed with error: " .. tostring(err))
+function SellBricks()
+    local Successful, Error =
+        pcall(function()
+            Remotes.sellBricks:FireServer()
+        end
+    )
+    if not Successful then
+        warn("Sell bricks failed with error: " .. tostring(Error))
+    end
 end
 
-local coins = 4800000000
-local iterCount = 100000
-
-local gsucc, gerr =
-    pcall(
-    function()
-        for i = 1, iterCount do
-            game:GetService("ReplicatedStorage").Remotes.generateBoost:FireServer("Coins", coins)
+function GenerateBoost()
+local Coins = 4800000000
+local Iterations = 100000
+local successful, error =
+    pcall(function()
+        for i = 1, Iterations do
+            Remotes.generateBoost:FireServer("Coins", Coins)
         end
-    end
-)
-
-if not gsucc then
-    warn("Generate coins failed with error: " .. tostring(gerr))
+    end)
+end
+if not successful then
+    warn("Generate Coins failed with error: " .. tostring(error))
 end
 
 function SetGunsStats(CoolDown, RocketSpeed)
-    local GStats = require(game:GetService("Players").LocalPlayer.Backpack.Launcher.Stats)
+    local GStats = require(LocalPlayer.Backpack.Launcher.Stats)
     GStats.Cooldown = CoolDown
     GStats.RocketSpeed = RocketSpeed
 end
 
 function BombCoolDown(CoolDown)
-    local BStats = require(game:GetService("Players").LocalPlayer.Backpack.Bomb.Stats)
+    local BStats = require(LocalPlayer.Backpack.Bomb.Stats)
     BStats.Cooldown = CoolDown
 end
+
+Chatted:Connect(function(Message)
+    local Arguments = string.split(Message, " ")
+    local CommandUsed = string.sub(Arguments[1], #Prefixs + 1):lower()
+
+    if CommandUsed == "cmds" then
+        print("bombcooldown - cooldown")
+        print("gunstats - cooldown, rocketspeed")
+        print("gaincoins")
+        print("sellbricks")
+    elseif CommandUsed == "bombcooldown" then
+        BombCoolDown(Arguments[2])
+    elseif CommandUsed == "gunstats" then
+        SetGunsStats(Arguments[2], Arguments[3])
+    elseif CommandUsed == "gaincoins" then
+        GenerateBoost()
+    elseif CommandUsed = "sellbricks" then
+        SellBricks()
+    end
+end)
