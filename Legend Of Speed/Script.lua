3acde15534b9a634e0b114e86be5f50254a05232
Styx <108234984+StyxDeveloper@users.noreply.github.com>
2024-01-10 12:00:46 -0500

    Removed
    Use another script

--- Legend Of Speed/Script.lua
+++
@@ -1,8 +0,0 @@
-while task.wait(0.6) do
-    for i = 1, 1000 do
-        local rEvents = game:GetService("ReplicatedStorage").rEvents
-        rEvents.orbEvent:FireServer("collectOrb", "Blue Orb", "City")
-        rEvents.orbEvent:FireServer("collectOrb", "Gem", "City")
-    end
-    game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer("rebirthRequest")
-end
