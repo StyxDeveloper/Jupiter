2f55377ae91efb52725a6d7a7818c314a11243d5
StyxDeveloper <niosfodsjfsfdsfds@gmail.com>
2024-02-03 18:10:25 -0500

    Jupiter Updated

--- Prison Life V2/Prison Life V2.lua
+++ Prison Life V2/Prison Life V2.lua
@@ -1,1 +1,21 @@
---! WIP
+--[[ Credits
+    !! // The Jupiter Staff Team // !!
+      *The Team*
+    // Styx Developer // 
+    // Lolegic //
+]]
+
+--! Global Variables !-- 
+
+
+--! Global Tables !-- 
+
+
+--! Checks !-- 
+game.Loaded:Wait()
+
+assert(not IsJupiterLoaded, "Jupiter is already loaded")
+getgenv().IsJupiterLoaded = true
+
+--! Functions !--
+
